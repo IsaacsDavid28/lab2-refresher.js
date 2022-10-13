c871a68f71ba0f7bac4871c4d84b57a78ab80e2d
David <isaacsdavid28@gmail.com>
2022-10-13 18:29:58 -0400

    first commit

---
+++ refresher.js
@@ -0,0 +1,25 @@
+let firstName = "David";
+
+let lastName = "Isaacs";
+
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+let population = 100000;
+
+console.log(population);
+
+console.log(population*3);
+
+let javaScriptIsCool = true;
+
+let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+for( var i = 1; i <= 5; i++){
+   console.log(i);
+}
+
+for( var i = 0; i <= 4; i++){
+   console.log(i);
+}
